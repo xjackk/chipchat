e4d889c75923890c1fdd2a0f0c79aebb2574708c
xjackk <xkillilea@gmail.com>
2017-11-30 12:59:38 -0500

    whatever

---
+++ app/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+
+    /**
+     * Fields that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = ['message'];    
+
+}
